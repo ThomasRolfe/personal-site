4d17fe2a3b571c54ca8158340870ee5d0cdaf8cc
Tom Rolfe <thomasrolfe@yahoo.co.uk>
2020-06-17 01:18:33 +0100

    Changed site logo to svg, changed header to purple

---
+++ src/components/Logo.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Logo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 301.89 134"
+    height="100%"
+  >
+    <defs>
+      <linearGradient
+        id="a"
+        x1="29"
+        y1="83.5"
+        x2="255"
+        y2="83.5"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stop-color="#d7baff" />
+        <stop offset=".24" stop-color="#983fd4" />
+        <stop offset=".38" stop-color="#7700bd" />
+        <stop offset=".45" stop-color="#6d00ae" />
+        <stop offset=".6" stop-color="#540085" />
+        <stop offset=".8" stop-color="#2b0045" />
+        <stop offset="1" />
+      </linearGradient>
+      <linearGradient
+        id="b"
+        y1="90.5"
+        x2="255"
+        y2="90.5"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stop-color="#d7baff" />
+        <stop offset=".24" stop-color="#913fcf" />
+        <stop offset=".38" stop-color="#6c00b6" />
+        <stop offset=".46" stop-color="#6300a7" />
+        <stop offset=".61" stop-color="#4b007e" />
+        <stop offset=".82" stop-color="#25003e" />
+        <stop offset="1" />
+      </linearGradient>
+    </defs>
+    <g data-name="Layer 2">
+      <g data-name="Layer 1">
+        <path d="M110.74 30h-49l6.07-30h127.81l-6.12 30h-40.84l-20.87 104H89.86z" />
+        <path d="M264.76 93.8l22.78 40.2h-39.37L228 98h-19.31l-7.16 36h-37.65l26.8-134h57.23c34.27 0 54 15.89 54 42.69-.02 24.69-13.8 43.07-37.15 51.11zM242.53 30h-19.79l-8 39h22.46c16.55 0 26.25-8 26.25-22.64 0-11.22-7.99-16.36-20.92-16.36z" />
+        <path fill="url(#a)" d="M29 98h226V69H34.19L29 98z" />
+        <path fill="url(#b)" d="M0 98h255V83H2.35L0 98z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default Logo;
--- src/components/navbar/SiteLogo.js
+++ src/components/navbar/SiteLogo.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../Logo";
 
 const SiteLogo = (props) => (
   <Link to="/" onClick={props.onClick}>
-    <img
+    {/* <img
       src="/images/logo-small.png"
       alt="Tom Rolfe Logo"
       className={`h-full py-3 fill-current text-offblack ${props.className}`}
-    />
+    /> */}
+    <div className="py-3 h-full">
+      <Logo className="" />
+    </div>
   </Link>
 );
 
