a3d2de8611a4397c668662f05ce2f2176b0f8278
Tom Rolfe <thomasrolfe@yahoo.co.uk>
2021-03-07 17:13:54 +0000

    Add build to repo

---
+++ build/precache-manifest.966e0a15c4f408bd5d90533ae87d6f9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a92ef80c8fea0abe6a71bfa3c5665d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fbefc44268d52a28790",
+    "url": "/static/css/main.8ff5c84d.chunk.css"
+  },
+  {
+    "revision": "fccac53c7bf35ee6c54b",
+    "url": "/static/js/2.9d8b068f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9d8b068f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbefc44268d52a28790",
+    "url": "/static/js/main.ccad0215.chunk.js"
+  },
+  {
+    "revision": "beeb01395cabb73d8210",
+    "url": "/static/js/runtime-main.e50a12a2.js"
+  },
+  {
+    "revision": "a2d43255997cc9df43d905f513d9ca8b",
+    "url": "/static/media/hero-animation.a2d43255.svg"
+  }
+]);
